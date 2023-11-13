237b7b583fdec5d6b33978e104b4017444c98113
Karen Keiko <nascikeiko@gmail.com>
2023-11-13 15:24:23 -0300

    feat(footer): Finalize the component with the triangles in the correct layout.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,54 +8,58 @@ export default function Footer() {
   const email = 'tesseract@email.com.br';
 
   return (
-    <footer className="relative bottom-0 left-0 h-404 bg-opacity-20 bg-gray">
-      <div className="flex flex-row-reverse justify-between pl-8">
-        <div>
-          <nav className="flex pt-20 pr-44 pb-8 pl-10 md:max-xl:pr-16">
-            <ul className="flex">
-              <li className="p-2 text-xl uppercase">
-                <a href="/">Quem Somos</a>
-              </li>
-              <li className="p-2 text-xl uppercase">
-                <a href="/">Serviços</a>
-              </li>
-              <li className="p-2 text-xl uppercase">
-                <a href="/">Projetos</a>
-              </li>
-              <li className="p-2 text-xl uppercase">
-                <a href="/">Carreira</a>
-              </li>
-              <li className="p-2 text-xl uppercase">
-                <a href="/">Blog</a>
-              </li>
-              <li className="p-2 text-xl uppercase">
-                <a href="/">Contato</a>
-              </li>
-            </ul>
-          </nav>
-          <div className="flex justify-between border-t-4 border-lightBlue pt-10 pr-40 pb-10 pl-10  md:max-xl:pr-20">
-            <div className="pl-2">
-              <h3 className="20px">{contact}</h3>
-              <a
-                href={`mailto:${email}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="email"
-              >
-                {` ${email}`}
-              </a>
+    <>
+      <footer className="relative bottom-0 left-0 h-404 bg-opacity-20 bg-gray">
+        <Triangles />
+        <div className="flex flex-row-reverse justify-between pl-8">
+          <div>
+            <nav className="flex pt-20 pr-44 pb-8 pl-10 md:max-xl:pr-16">
+              <ul className="flex">
+                <li className="p-2 text-xl uppercase">
+                  <a href="/">Quem Somos</a>
+                </li>
+                <li className="p-2 text-xl uppercase">
+                  <a href="/">Serviços</a>
+                </li>
+                <li className="p-2 text-xl uppercase">
+                  <a href="/">Projetos</a>
+                </li>
+                <li className="p-2 text-xl uppercase">
+                  <a href="/">Carreira</a>
+                </li>
+                <li className="p-2 text-xl uppercase">
+                  <a href="/">Blog</a>
+                </li>
+                <li className="p-2 text-xl uppercase">
+                  <a href="/">Contato</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="flex justify-between text-xl border-t-4 border-lightBlue pt-10 pr-40 pb-10 pl-10  md:max-xl:pr-20">
+              <div className="pl-2">
+                <h3 className="20px">{contact}</h3>
+                <a
+                  href={`mailto:${email}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="email"
+                >
+                  {` ${email}`}
+                </a>
+              </div>
+              <SocialMediaIcons />
             </div>
-            <SocialMediaIcons />
+          </div>
+          <div className="pt-16 pl-40 md:max-xl:pl-24">
+            <Logo width={322} height={222} />
           </div>
         </div>
-        <div className="pt-16 pl-40 md:max-xl:pl-24">
-          <Logo width={322} height={222} />
+        <div>
+          <p className="flex justify-end text-xl pt-2 pr-44 relative text-white opacity-50 md:max-xl:pr-16">
+            {copyRight}
+          </p>
         </div>
-      </div>
-      <Triangles />
-      <div>
-        <p className="flex justify-end pt-2 pr-44 text-white/50">{copyRight}</p>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
--- app/components/Triangles.tsx
+++ app/components/Triangles.tsx
@@ -1,7 +1,7 @@
 export default function Triangulos() {
   return (
     <>
-      <div className="triangle1 border-lightBlue"></div>
+      <div className="triangle1"></div>
       <div className="triangle2"></div>
       <div className="triangle3"></div>
     </>
