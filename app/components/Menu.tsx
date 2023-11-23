07021fd177b2d9a1c601539dfc7fab84624d95f7
Karen Keiko <nascikeiko@gmail.com>
2023-11-23 10:12:39 -0300

    feat(header): (WIP) Adds the Header component containing the Menu.

---
+++ app/components/Menu.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Image from 'next/image';
+import iconMenu from '../../assets/image/iconMenu.svg';
+import React, { useState } from 'react';
+
+export default function Menu() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <div className="flex justify-end">
+      <div
+        className={!openMenu ? 'pt-16 pr-36 pb-24' : 'hidden'}
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <Image src={iconMenu} alt="menu" width={50} height={50} />
+      </div>
+
+      {openMenu && (
+        <>
+          <button
+            className="flex absolute w-[100px] h-[100px] m-8"
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <svg
+              className="h-8 w-8 text-gray-600"
+              viewBox="0 0 48 48"
+              fill="none"
+              stroke="#23AFB3"
+              strokeWidth="1"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </button>
+          <div className="bg-lightBlue bg-opacity-80 w-[67%] blur-1"></div>
+          <nav className="flex flex-col flex-wrap w-[33%] pt-12 pr-24 pb-36 pl-16 bg-gray bg-opacity-20">
+            <ul className="flex flex-col justify-between min-h-[250px] mt-48">
+              <li className="text-white text-3xl font-black my-2 uppercase">
+                <a href="/quemSomos">Quem Somos</a>
+              </li>
+              <li className="text-white text-3xl font-black my-2 uppercase">
+                <a href="/servicos">Serviços</a>
+              </li>
+              <li className="text-white text-3xl font-black my-2 uppercase">
+                <a href="/projetos">Projetos</a>
+              </li>
+              <li className="text-white text-3xl font-black my-2 uppercase">
+                <a href="/carreira">Carreira</a>
+              </li>
+              <li className="text-white text-3xl font-black my-2 uppercase">
+                <a href="/blog">Blog</a>
+              </li>
+              <li className="text-white text-3xl font-black my-2 uppercase">
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </>
+      )}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
 import Footer from './components/Footer';
+import Header from './components/Header';
 import Section from './components/Section';
 import TeamInformation from './components/TeamInformation';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="text-2xl font-bold">Grupo Tesseract</div>
-      <section className="min-w-full">
-        <div className="min-w-full">
-          <Section />
+    <>
+      <header>
+        <Header />
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <section className="min-w-full">
+          <div className="min-w-full">
+            <Section />
+          </div>
+          <div className="min-w-full">
+            <TeamInformation />
+          </div>
+        </section>
+        <div className="min-w-full pt-72">
+          <Footer />
         </div>
-        <div className="min-w-full">
-          <TeamInformation />
-        </div>
-      </section>
-      <div className="min-w-full pt-72">
-        <Footer />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
