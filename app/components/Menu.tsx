d52723cdd463b06d5e896756e867bf7b3f41b0aa
Karen Keiko <nascikeiko@gmail.com>
2024-02-22 10:38:11 -0300

    feat(state): Adjusts the state management of the Menu component.

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -2,61 +2,105 @@
 
 import Image from 'next/image';
 import iconMenu from '../../assets/image/iconMenu.svg';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 
 export default function Menu() {
   const [openMenu, setOpenMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const toglleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(e: { target: any }) {
+      if (
+        (menuRef.current as HTMLElement) &&
+        !(menuRef.current as HTMLElement).contains(e.target)
+      ) {
+        setOpenMenu(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="flex justify-end h-screen w-screen">
       <div
         className={!openMenu ? 'pt-16 pr-36 pb-24' : 'hidden'}
-        onClick={() => setOpenMenu(!openMenu)}
+        onClick={toglleMenu}
       >
         <Image src={iconMenu} alt="menu" width={50} height={50} />
       </div>
 
       {openMenu && (
         <>
-          <button
-            className="flex absolute w-24 h-24 m-8"
-            onClick={() => setOpenMenu(!openMenu)}
-          >
-            <svg
-              className="h-24 w-20 text-gray-600"
-              viewBox="0 0 48 48"
-              fill="none"
-              stroke="#23AFB3"
-              strokeWidth="1"
+          <div className="flex justify-end w-screen relative">
+            <div className="bg-lightBlue bg-opacity-80 w-2/3 blur-1"></div>
+            <button
+              className="flex absolute w-24 h-24 m-8"
+              onClick={() => setOpenMenu(!openMenu)}
             >
-              <line x1="18" y1="6" x2="6" y2="18" />
-              <line x1="6" y1="6" x2="18" y2="18" />
-            </svg>
-          </button>
-          <div className="bg-lightBlue bg-opacity-80 w-2/3 blur-1"></div>
-          <nav className="flex flex-col flex-wrap w-1/3 pt-12 pr-24 pb-36 pl-20">
-            <ul className="flex flex-col justify-between h-60 mt-48 relative">
-              <li className="text-white text-3xl font-black my-2 uppercase">
-                <Link href="/quemsomos">Quem Somos</Link>
-              </li>
-              <li className="text-white text-3xl font-black my-2 uppercase">
-                <Link href="/servicos">Serviços</Link>
-              </li>
-              <li className="text-white text-3xl font-black my-2 uppercase">
-                <Link href="/projetos">Projetos</Link>
-              </li>
-              <li className="text-white text-3xl font-black my-2 uppercase">
-                <Link href="/carreira">Carreira</Link>
-              </li>
-              <li className="text-white text-3xl font-black my-2 uppercase">
-                <Link href="/blog">Blog</Link>
-              </li>
-              <li className="text-white text-3xl font-black my-2 uppercase">
-                <Link href="/contato">Contato</Link>
-              </li>
-            </ul>
-          </nav>
+              <svg
+                className="h-24 w-20 text-gray-600"
+                viewBox="0 0 48 48"
+                fill="none"
+                stroke="#23AFB3"
+                strokeWidth="1"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </button>
+            <nav
+              ref={menuRef}
+              className="menu-nav flex flex-col flex-wrap w-1/3"
+            >
+              <ul className="flex flex-col justify-between mt-48 relative">
+                <li
+                  onClick={toglleMenu}
+                  className="text-white text-3xl font-black my-2 uppercase"
+                >
+                  <Link href="/quemsomos">Quem Somos</Link>
+                </li>
+                <li
+                  onClick={toglleMenu}
+                  className="text-white text-3xl font-black my-2 uppercase"
+                >
+                  <Link href="/servicos">Serviços</Link>
+                </li>
+                <li
+                  onClick={toglleMenu}
+                  className="text-white text-3xl font-black my-2 uppercase"
+                >
+                  <Link href="/projetos">Projetos</Link>
+                </li>
+                <li
+                  onClick={toglleMenu}
+                  className="text-white text-3xl font-black my-2 uppercase"
+                >
+                  <Link href="/carreira">Carreira</Link>
+                </li>
+                <li
+                  onClick={toglleMenu}
+                  className="text-white text-3xl font-black my-2 uppercase"
+                >
+                  <Link href="/blog">Blog</Link>
+                </li>
+                <li
+                  onClick={toglleMenu}
+                  className="text-white text-3xl font-black my-2 uppercase"
+                >
+                  <Link href="/contato">Contato</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </>
       )}
     </div>
