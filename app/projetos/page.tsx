53ea0f4599ec977ee9968eb713b0d7850b06a0e6
Karen Keiko <nascikeiko@gmail.com>
2024-02-28 15:57:25 -0300

    feat(reusable): Create a reusable component that contains the titles of the pages.

--- app/projetos/page.tsx
+++ app/projetos/page.tsx
@@ -1,7 +1,27 @@
+import PageTitle from '../components/PageTitle';
+
+const boxData = [
+  {
+    subtitle1: 'Sites.Sistemas.Aplicativos',
+    subtitleClass: 'pl-1',
+    title: 'Projetos',
+    key: 1,
+  },
+];
+
 export default function Projects() {
   return (
     <>
-      <div>Projetos</div>
+      {boxData.map(function (item) {
+        return (
+          <PageTitle
+            subtitle1={item.subtitle1}
+            subtitleClass={item.subtitleClass}
+            title={item.title}
+            key={item.key}
+          />
+        );
+      })}
     </>
   );
 }
--- app/servicos/page.tsx
+++ app/servicos/page.tsx
@@ -1,8 +1,34 @@
+import PageTitle from '../components/PageTitle';
 import TeamInformation from '../components/TeamInformation';
+
+const boxData = [
+  {
+    subtitle1: 'Outsourcing',
+    subtitle2: 'Desenvolvimento de Sistemas',
+    subtitle3: 'Consultoria',
+    subtitleClass: 'pl-1',
+    title: 'Nossos Serviços',
+    key: 1,
+  },
+];
 
 export default function Services() {
   return (
     <>
+      <div>
+        {boxData.map(function (item) {
+          return (
+            <PageTitle
+              subtitle1={item.subtitle1}
+              subtitle2={item.subtitle2}
+              subtitle3={item.subtitle3}
+              subtitleClass={item.subtitleClass}
+              title={item.title}
+              key={item.key}
+            />
+          );
+        })}
+      </div>
       <div className="min-w-full">
         <TeamInformation />
       </div>
