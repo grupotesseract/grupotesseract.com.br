6049a47368b5e54ca0463a2cc9cf442635f412a2
Karen Keiko <nascikeiko@gmail.com>
2024-03-13 16:34:12 -0300

    feat(card-page): Add the button and text at the bottom of the page.

--- app/quemsomos/page.tsx
+++ app/quemsomos/page.tsx
@@ -1,11 +1,15 @@
 import CompanyCulture from '../components/CompanyCulture';
 import TeamMemberList from '../components/TeamMemberList';
+import TextWithButton from '../components/TextWithButton';
 
 export default function AboutUs() {
   return (
     <>
-      <CompanyCulture />
-      <TeamMemberList />
+      <div className="container">
+        <CompanyCulture />
+        <TeamMemberList />
+        <TextWithButton />
+      </div>
     </>
   );
 }
