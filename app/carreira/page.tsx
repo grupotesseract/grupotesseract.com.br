d0ebd5b441aa4e28c707bca243971a53b538ab80
Karen Keiko <nascikeiko@gmail.com>
2024-04-22 17:53:48 -0300

    feat(icon-circle): Create component that represents icons in circles.

--- app/carreira/page.tsx
+++ app/carreira/page.tsx
@@ -1,10 +1,59 @@
+import IconText from '../components/IconText';
+import contact from '@/assets/image/contact.svg';
+
 export default function Career() {
+  const data = [
+    {
+      icon: contact,
+      iconClass: 'w-40 h-40',
+      text: 'Cultura profissional colaborativa',
+      textClass: 'text-2xl font-bold text-gray pl-4',
+      key: 1,
+    },
+    {
+      icon: contact,
+      iconClass: 'w-40 h-40',
+      text: 'Sistema de apoio interno',
+      textClass: 'text-2xl font-bold text-gray pl-4',
+      key: 2,
+    },
+    {
+      icon: contact,
+      iconClass: 'w-40 h-40',
+      text: 'Variedade de tecnologias',
+      textClass: 'text-2xl font-bold text-gray pl-4',
+      key: 3,
+    },
+    {
+      icon: contact,
+      iconClass: 'w-40 h-40',
+      text: 'Trabalho 100% remoto',
+      textClass: 'text-2xl font-bold text-gray pl-4',
+      key: 4,
+    },
+  ];
   return (
     <>
       <div>
         <h1 className="title-page flex justify-center mt-52 font-extrabold text-white">
           Se junte a nossa equipe
         </h1>
+      </div>
+      <div className="flex items-center mt-52 gap-8">
+        {data.map((item) => {
+          return (
+            <IconText
+              key={item.key}
+              icon={item.icon.src}
+              iconClass={item.iconClass}
+              text={item.text}
+              textClass={item.textClass}
+              iconName={'icone'}
+              width={166}
+              height={166}
+            />
+          );
+        })}
       </div>
     </>
   );
--- app/quemsomos/page.tsx
+++ app/quemsomos/page.tsx
@@ -1,12 +1,60 @@
 import CompanyCulture from '../components/CompanyCulture';
+import IconText from '../components/IconText';
 import TeamMemberList from '../components/TeamMemberList';
 import TextWithButton from '../components/TextWithButton';
+import contact from '@/assets/image/contact.svg';
 
 export default function AboutUs() {
+  const data = [
+    {
+      icon: contact,
+      iconClass: 'w-48 h-48',
+      text: 'Cultura profissional colaborativa',
+      textClass: 'text-2lg font-bold text-gray pl-4',
+      key: 1,
+    },
+    {
+      icon: contact,
+      iconClass: 'w-48 h-48',
+      text: 'Sistema de apoio interno',
+      textClass: 'text-2lg font-bold text-gray pl-4',
+      key: 2,
+    },
+    {
+      icon: contact,
+      iconClass: 'w-48 h-48',
+      text: 'Variedade de tecnologias',
+      textClass: 'text-2lg font-bold text-gray pl-4',
+      key: 3,
+    },
+    {
+      icon: contact,
+      iconClass: 'w-48 h-48',
+      text: 'Trabalho 100% remoto',
+      textClass: 'text-2lg font-bold text-gray pl-4',
+      key: 4,
+    },
+  ];
   return (
     <>
       <div>
         <CompanyCulture />
+        <div className="flex justify-center items-center gap-24 mt-52 pl-2 pr-2">
+          {data.map((item) => {
+            return (
+              <IconText
+                key={item.key}
+                icon={item.icon.src}
+                iconClass={item.iconClass}
+                text={item.text}
+                textClass={item.textClass}
+                iconName={'icone'}
+                width={195}
+                height={195}
+              />
+            );
+          })}
+        </div>
         <TeamMemberList />
         <TextWithButton />
       </div>
