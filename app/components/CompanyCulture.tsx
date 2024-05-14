1c8f72a644cc6561ae64d067c8d1f89619bd9ad2
Karen Keiko <nascikeiko@gmail.com>
2024-05-14 17:29:17 -0300

    feat(layout): Adjusts layout responsive About us page.

--- app/components/CompanyCulture.tsx
+++ app/components/CompanyCulture.tsx
@@ -1,15 +1,13 @@
 export default function CompanyCulture() {
   return (
     <>
-      <div className="box flex"></div>
-      <div className="culture-box flex justify-center items-end">
-        <div className="flex">
-          <h1 className="culture-title uppercase text-white">
-            Grupo <br /> Tesseract
-          </h1>
-        </div>
-        <div className="flex flex-col justify-center items-start gap-16 relative">
-          <div className="culture-text flex mr-16">
+      <div className="box w-2/5 hidden xl:flex"></div>
+      <div className="culture-box flex flex-col justify-center items-end xl:flex-row md:max-xl:items-center md:max-xl:gap-16 md:max-xl:pt-40">
+        <h1 className="culture-title uppercase text-white md:max-xl:pr-72 md:max-xl:text-6xl">
+          Grupo <br /> Tesseract
+        </h1>
+        <div className="flex flex-col justify-center items-start gap-16 relative md:max-xl:gap-8">
+          <div className="culture-text flex mr-16 md:max-xl:text-xl md:max-xl:mr-28">
             <p className="font-extrabold">
               &quot;Estamos procurando uma
               <br />
@@ -20,7 +18,7 @@ export default function CompanyCulture() {
               de desenvolver softwares &quot;
             </p>
           </div>
-          <div className="culture-description font-normal text-white">
+          <div className="culture-description font-normal text-white md:max-xl:text-xl">
             <p>
               <span className="font-bold">O TESSERACT</span> é um grupo de
               profissionais da área de TI que acredita na importância da
--- app/components/GenericButton.tsx
+++ app/components/GenericButton.tsx
@@ -10,7 +10,7 @@ export default function GenericButton(props: GenericButtonProps) {
   return (
     <Link href={props.path}>
       <button
-        className={`${props.buttonClass} w-96 h-20 bg-gray hover:bg-slate-400 font-bold uppercase`}
+        className={`${props.buttonClass} w-72 h-10 bg-gray hover:bg-slate-400 font-bold uppercase xl:w-96 xl:h-20`}
       >
         {props.children}
       </button>
