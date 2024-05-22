1354a765e29fd2c5538f63e35e6068e87610f180
Karen Keiko <nascikeiko@gmail.com>
2024-05-21 22:21:12 -0300

    feat(layout): Adjusts layout responsive Footer and title of the Career page.

--- app/carreira/page.tsx
+++ app/carreira/page.tsx
@@ -2,7 +2,7 @@ export default function Career() {
   return (
     <>
       <div>
-        <h1 className="title-page flex justify-center mt-52 font-extrabold text-white">
+        <h1 className="title-page flex justify-center mt-20 text-4xl font-extrabold text-white uppercase lg:mt-52 lg:text-7xl">
           Se junte a nossa equipe
         </h1>
       </div>
