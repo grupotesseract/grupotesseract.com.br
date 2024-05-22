543d3ac7bd0dcc1179c8378ef3156e7ecd7bf636
Karen Keiko <nascikeiko@gmail.com>
2024-05-21 22:53:23 -0300

    feat(layout): Adjust the responsive of the title on the services page.

--- app/components/PageTitle.tsx
+++ app/components/PageTitle.tsx
@@ -5,13 +5,14 @@ interface PageTitleProps {
   subtitle3?: string;
   subtitleClass?: string;
   title?: string;
+  titleClass?: string;
 }
 
 export default function PageTitle(props: PageTitleProps) {
   return (
     <div className="box-title flex">
-      <div className="flex flex-col justify-center pl-32">
-        <div className={`flex gap-4 ${props.subtitleClass}`}>
+      <div className="flex flex-col justify-center pl-8 lg:pl-32">
+        <div className={`flex gap-4 mb-8 lg:mb-0 ${props.subtitleClass}`}>
           <h3 className="text-xl font-bold text-lightBlue uppercase">
             {props.subtitle1}
           </h3>
@@ -22,7 +23,11 @@ export default function PageTitle(props: PageTitleProps) {
             {props.subtitle3}
           </h3>
         </div>
-        <h1 className="title-page font-extrabold text-white">{props.title}</h1>
+        <h1
+          className={`font-extrabold uppercase text-white ${props.titleClass} lg:text-7xl`}
+        >
+          {props.title}
+        </h1>
       </div>
     </div>
   );
