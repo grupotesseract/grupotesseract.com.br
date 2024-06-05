9668528c9d40c93d39bc86c575a5b1efc10412e3
Karen Keiko <nascikeiko@gmail.com>
2024-06-04 23:17:48 -0300

    fix: Fixed layout mobile About us page.

--- app/components/CompanyCulture.tsx
+++ app/components/CompanyCulture.tsx
@@ -2,12 +2,12 @@ export default function CompanyCulture() {
   return (
     <>
       <div className="box w-2/5 hidden xl:flex"></div>
-      <div className="culture-box flex flex-col justify-center items-end xl:flex-row md:max-xl:items-center md:max-xl:gap-16 md:max-xl:pt-40">
-        <h1 className="culture-title uppercase text-white md:max-xl:pr-72 md:max-xl:text-6xl">
+      <div className="culture-box flex flex-col justify-center items-center pt-20 xl:flex-row md:gap-16 lg:pt-56">
+        <h1 className="culture-title uppercase text-white text-6xl lg:pr-40 md:text-7xl">
           Grupo <br /> Tesseract
         </h1>
-        <div className="flex flex-col justify-center items-start gap-16 relative md:max-xl:gap-8">
-          <div className="culture-text flex mr-16 md:max-xl:text-xl md:max-xl:mr-28">
+        <div className="flex flex-col justify-center items-start gap-16 relative p-8 md:p-0 md:gap-8">
+          <div className="culture-text flex text-2xl md:mr-28 md:text-4xl">
             <p className="font-extrabold">
               &quot;Estamos procurando uma
               <br />
@@ -18,7 +18,7 @@ export default function CompanyCulture() {
               de desenvolver softwares &quot;
             </p>
           </div>
-          <div className="culture-description font-normal text-white md:max-xl:text-xl">
+          <div className="culture-description text-2xl font-normal text-white md:text-3xl">
             <p>
               <span className="font-bold">O TESSERACT</span> é um grupo de
               profissionais da área de TI que acredita na importância da
